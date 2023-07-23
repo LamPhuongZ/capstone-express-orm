7dae19d6ac2953e564fe25260bd27b79351da498
LamPhuongZ <lamphuong024@gmail.com>
2023-07-24 01:19:54 +0700

    update get user by token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,20 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Headers, HttpException } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtService } from '@nestjs/jwt';
 
+@ApiBearerAuth()
 @UseGuards(AuthGuard("jwt"))
 @ApiTags("User")
 @Controller('/user')
 export class UserController {
   constructor(
-    private readonly userService: UserService,
-    private jwtService: JwtService
+    private readonly userService: UserService
   ) { }
 
   @Get("/get-user")
   getUser(@Headers("Authorization") token: string) {
-    // try {
-    //   if (this.jwtService.verify(token)) {
-    return this.userService.getUserByToken(token)
-    //   }
-    //   else {
-    //     throw new HttpException("Unauthorized", 401);
-    //   }
-    // } catch (error) {
-    //   throw new HttpException(error.response, error.status);
-
-    // }
+    return this.userService.getUserByToken(token);
   }
 }
